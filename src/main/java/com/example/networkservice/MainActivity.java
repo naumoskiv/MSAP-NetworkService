94cf8b68d4eee96e09bfdd048b7def074aa9dbe3
naumoskiv <58512274+naumoskiv@users.noreply.github.com>
2021-05-09 22:28:36 +0200

    Initial commit

---
+++ src/main/java/com/example/networkservice/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.networkservice;
+
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.networkservice.restarter.RestartServiceBroadcastReceiver;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            RestartServiceBroadcastReceiver.scheduleJob(getApplicationContext());
+        } else {
+            ProcessMainClass bck = new ProcessMainClass();
+            bck.launchService(getApplicationContext());
+        }
+    }
+}
